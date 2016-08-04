db0b3489531bbcc627b942281f9bb6fec4bc7b73
Vincent Duong <vincentduong@Vincents-MacBook-Pro.local>
2016-08-04 10:58:46 -0700

    Reorganize click function into jquery function

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 $(document).ready(function() {
-	console.log("ready");
+
+	$("#btnCalculate").click(function(){
+		// M = P [ (r * (1+r)^n) / (((1+r)^n) - 1) ]
+
+		var loanBalance = $("#loanBalance").val();				// P
+		var interestRate = $("#interestRate").val();			// Annual interest rate
+		var loanTerm = $("#loanTerm").val();
+		var period = $("#period option:selected").val();	
+
+		var numOfPayments = loanTerm * period;							// n 
+		var monthlyInterestRate = (interestRate / 100) / period;		// r
+		var compoundedInterestRate = Math.pow((1 + monthlyInterestRate), numOfPayments);	// (1+r)^n
+		var interestQuotient = (monthlyInterestRate * compoundedInterestRate) / (compoundedInterestRate - 1);  // (r*(1+r)^n)/(((1+r)^n)-1)
+		var monthlyPayment = Math.round((loanBalance * interestQuotient) * 100) / 100;
+
+		$("#output").text("Your monthly payment is $" + monthlyPayment + ".");
+	});
 });
 
-$("#btnCalculate").click(function(){
-	// M = P [ (r * (1+r)^n) / (((1+r)^n) - 1) ]
-
-	var loanBalance = $("#loanBalance").val();				// P
-	var interestRate = $("#interestRate").val();			// Annual interest rate
-	var loanTerm = $("#loanTerm").val();
-	var period = $("#period option:selected").val();	
-
-	var numOfPayments = loanTerm * period;							// n 
-	var monthlyInterestRate = (interestRate / 100) / period;		// r
-	var compoundedInterestRate = Math.pow((1 + monthlyInterestRate), numOfPayments);	// (1+r)^n
-	var interestQuotient = (monthlyInterestRate * compoundedInterestRate) / (compoundedInterestRate - 1);  // (r*(1+r)^n)/(((1+r)^n)-1)
-	var monthlyPayment = Math.round((loanBalance * interestQuotient) * 100) / 100;
-
-	$("#output").text("Your monthly payment is $" + monthlyPayment + ".");
-});
